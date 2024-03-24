08f0f2c8d6fba9981ee91961a035ca426f1eafea
waltersimekha <waltersimekha@gmail.com>
2024-03-25 00:02:51 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,22 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)
+
+print("Delete Payroll")
+payroll.delete()
+print(payroll)
+
 ipdb.set_trace()
